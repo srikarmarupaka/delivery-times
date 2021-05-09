8678e0f6eb9bab3cb38f922ee8c11f24fa17c0ee
srikarsharmamarpaka@gmail.com <srikar@muzigal.com>
2021-05-09 23:23:02 +0530

    added comments

--- src/cubeWealth/ReadCSV.java
+++ src/cubeWealth/ReadCSV.java
@@ -33,45 +33,57 @@ public class ReadCSV{
 			File file = new File("src/data.csv");
 			Scanner sc = new Scanner(file);
 			sc.nextLine();//skipping 1st line
+			
 			while(sc.hasNextLine()) { //looping if there exists a line
 				
+				//splitting the line into strings (split with ",")
 				String s = sc.nextLine();
 				String[] data = s.split(",");
 				
+				//parsing Time & Date
 				int sno = Integer.parseInt(data[0]);
 		        Instant pickupTimeInstant = Instant.parse(data[3]);
 		        LocalDateTime pickupTime = LocalDateTime.ofInstant(pickupTimeInstant, ZoneId.of(ZoneOffset.UTC.getId()));
 		        Instant deliveryTimeInstant = Instant.parse(data[4]);
 		        LocalDateTime deliveryTime = LocalDateTime.ofInstant(deliveryTimeInstant, ZoneId.of(ZoneOffset.UTC.getId()));
 		        
+		        //Creating a delivery object
 				Delivery D = new Delivery(sno, data[1], data[2], pickupTime, deliveryTime);
 				deliveryList.add(D);
 				tD = D.timeTaken();
-				
-//				Person P = new Person(data[1],tD);
+				//Creating a person object
 				Person P = new Person(data[1],tD,pickupTime, deliveryTime);
-
+				
+				//iterator initializations
 				int deli=0, peri=0;
 				boolean flag = false;
 				
+				//Looping until, deliveries complete
 				while(deli <= personList.size()) {
-					
+					//adding 1st delivery
 					if(peri==0 && deli==0) {
 						personList.add(P);
-					} 
+					}
+					
+					//checking if the name already exists
 					if(data[1].equals(personList.get(peri).personName)) {
+						//adding delivery, if name already exists.
 						personList.get(peri).noOfDeliveries++;
+						
+						//overlapping times conditions
 						if(personList.get(peri).deliveryTime.isAfter(pickupTime)) {
 							personList.get(peri).deliveryTime = pickupTime;
 							personList.get(peri).differenceTime();
 							tD = personList.get(peri).timeTaken();
 						}
+						
+						//calculating Total time-taken; considering overlapping times
 						personList.get(peri).totalTime = personList.get(peri).totalTime + tD;
 						if(maxTD < personList.get(peri).totalTime) {
 							maxTD = personList.get(peri).totalTime;
 							maxPerson = personList.get(peri).personName;
 						}
-						System.out.println(personList.get(peri).personName+" : "+personList.get(peri).totalTime+" : "+personList.get(peri).noOfDeliveries);
+
 						flag = false;
 						deli++;
 						break;
@@ -79,6 +91,7 @@ public class ReadCSV{
 					if(deli == personList.size()) flag = true;
 					peri++;
 				}
+				//adding person if not exists
 				if(flag) {
 					personList.add(P);
 					deli++;
@@ -89,7 +102,8 @@ public class ReadCSV{
 			}
 
 			Arrays.sort(timeDurations);
-//			System.out.println(timeDurations[timeDurations.length-1]+"min. is the max time taken for a delivery");
+			
+			//Final output
 			System.out.println(maxPerson +" has taken largest time, i.e.," +maxTD+"min. for his deliveries");
 			sc.close();
 		} catch(FileNotFoundException e) {
